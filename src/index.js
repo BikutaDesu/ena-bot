f858ac975de1aea158e35f782ca14099dc749826
Victor Neves <victor.neves4@fatec.sp.gov.br>
2021-07-16 22:48:32 -0300

    :art: update cooldown check structure Changes to be committed: 	modified:   src/index.js 	new file:   src/utils/checkCooldown.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Discord from 'discord.js';
 import dotenv from 'dotenv';
 import getCommands from './utils/getCommands.js';
+import checkCooldown from './utils/checkCooldown.js';
 import configs from '../config.json';
 
 dotenv.config();
@@ -25,30 +26,14 @@ client.on('message', (message) => {
 
   const command = client.commands.get(commandName);
 
-  const { cooldowns } = client;
-  if (!cooldowns.has(command.name)) {
-    cooldowns.set(command.name, new Discord.Collection());
-  }
-
-  const now = Date.now();
-  const timestamps = cooldowns.get(command.name);
-  const cooldownAmount = (command.cooldownAmount || 3) * 1000;
-
-  if (timestamps.has(message.author.id)) {
-    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-    if (now < expirationTime) {
-      const timeLeft = (expirationTime - now) / 1000;
-      return message.reply(
-        `por favor espere mais ${timeLeft.toFixed(
-          1
-        )} segundo(s) antes de usar o comando \`${command.name}\` novamente.`
-      );
-    }
-  }
-
-  timestamps.set(message.author.id, now);
-  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
+  const timeLeft = checkCooldown(client, command, message);
+  if (timeLeft) {
+    return message.reply(
+      `por favor espere mais ${timeLeft.toFixed(
+        1
+      )} segundo(s) antes de usar o comando \`${command.name}\` novamente.`
+    );
+  } 
 
   if (command.args && !args.length) {
     let reply = `Você não informou nenhum argumento, ${message.author}`;
---
+++ src/utils/checkCooldown.js
@@ -0,0 +1,24 @@
+import { Collection } from "discord.js";
+
+export default (client, command, message) => {
+  const { cooldowns } = client;
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldownAmount || 3) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return timeLeft;
+    }
+  }
+
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
+}
